9a0012ec1efa16538f1a91c615c44626c66705c7
blackmatch <coder.blackmatch@gmail.com>
2017-07-03 00:04:14 +0800

    publish 0.1.8

--- bin/downloader.js
+++ bin/downloader.js
@@ -3,13 +3,10 @@
 const Downloader = require('../lib/downloader');
 
 const main = function(url, outDir) {
-  console.log(`url: ${url}`);
-  console.log(`outDir: ${outDir}`);
   const download = new Downloader();
   download.get(url, outDir);
 };
 
-const url = process.argv[1];
-const outDir = process.argv[2];
-console.log(process.argv);
+const url = process.argv[2];
+const outDir = process.argv[3];
 main(url, outDir);
